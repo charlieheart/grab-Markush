87373b382a774f92b5981028820552918cdd3bc5
charlieheart <32141102+charlieheart@users.noreply.github.com>
2017-09-21 20:58:48 +0300

    Add files via upload

---
+++ GetImgs.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 13:50:08 2017
+
+@author: Александр
+"""
+import time
+import os
+import re
+import urllib.request
+from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait 
+#from selenium.webdriver.support import expected_conditions as EC 
+
+driver = webdriver.Firefox()
+with open ('urls.txt', mode = 'r') as file:
+    lines = file.readlines()
+
+for line in lines:
+    
+    driver.get(line)
+    
+    images = driver.find_elements_by_tag_name('img')
+    
+    for image in images:
+        src = image.get_attribute('src')
+        if 'fpimage' in src:
+            imgUrl = src
+    
+    imgUrl = imgUrl.replace("true", "false")
+    
+    Expression = re.compile('/\w*@')
+    
+    SearchObject = Expression.search(imgUrl)
+    
+    FoundString = SearchObject.group()
+    
+    FoundString = FoundString.replace('/', '')
+    PN = FoundString.replace('@', '')
+    
+    driver.get(imgUrl)
+    
+    image = driver.find_element_by_tag_name('img')
+    src = image.get_attribute('src')
+    fullfilename = os.path.join('E:\Smal Molecules\Pics', PN + '.png')
+    urllib.request.urlretrieve(src, fullfilename)
+
+        
+time.sleep(5)
+driver.quit()
---
+++ GetURLs.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 07:36:26 2017
+
+@author: Александр
+"""
+import time
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+
+entries = 25
+
+driver = webdriver.Firefox()
+driver.get('https://patentscope.wipo.int/search/en/search.jsf')
+search = driver.find_element_by_name('simpleSearchSearchForm:fpSearch')
+search.clear()
+search.send_keys('Formula (I)')
+search.send_keys(Keys.RETURN)
+
+
+element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((
+        By.ID, 'resultTable:0:resultListTableColumnLink')))
+
+ids = []
+open('urls.txt', 'w').close()
+
+def grablinks(n):
+    for i in range (n):
+        
+        ids.append ( 'resultTable:' + str(i) + ':resultListTableColumnLink')
+        linkElement = driver.find_element_by_id(ids[i])
+        url = linkElement.get_attribute('outerHTML')
+        Expression = re.compile('detail\.jsf\?docId=\w*&amp')
+        SearchObject = Expression.search(str(url))
+        FoundString = SearchObject.group()
+            
+        FoundString = FoundString.replace('&amp', '')
+        url = 'https://patentscope.wipo.int/search/en/' + FoundString
+        with open ('urls.txt', mode = 'a') as file:
+            file.write (url + '\n') 
+
+
+def nextpage():
+    nextElement = driver.find_element_by_xpath(
+            "//*[contains(text(), 'next')]")
+    nextElement.click()
+           
+
+while entries > 10:
+    grablinks(10)
+    nextpage()
+    entries -=10
+else:
+    grablinks(entries)
+    
+
+time.sleep(5)
+driver.quit()
